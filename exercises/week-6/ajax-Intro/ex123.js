89f4af35ae03461108478b3db3d80046209ccc07
nudefingers <nude.fingers@gmail.com>
2023-07-09 21:34:15 +0300

    done ajax intro

---
+++ exercises/week-6/ajax-Intro/ex123.js
@@ -0,0 +1,59 @@
+// Exercise 1+2+3
+let books = []
+let isbns = [9782806269171,
+    1440633908,
+    9781945048470,
+    9780307417138]
+
+
+const addBookData = function (_data) {
+    _data.items.forEach(function (_item) {
+        let _info = _item.volumeInfo
+        let author = _info.authors ? `, Author: ` + _info.authors[0] : ``
+        let _bookDiv = $(`<div>`).text(`Name: ` + _info.title + author)
+        $(`#books`).append(_bookDiv)
+    })
+}
+
+const search = function () {
+    isbns.forEach(function (isbn) {
+        $.ajax({
+            method: "GET",
+            url: `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`,
+            success: function (data) {
+                addBookData(data)
+                console.log(data)
+            },
+            error: function (xhr, text, error) {
+                console.log(text)
+            }
+        })
+    })
+}
+
+
+const fetch = function () {
+    books.forEach(function (book) {
+        let queryType = book.queryType
+        let queryValue = book.queryValue
+        console.log(queryType)
+        console.log(`888`)
+
+        $.ajax({
+            method: "GET",
+            url: `https://www.googleapis.com/books/v1/volumes?q=${queryType}:${queryValue}`,
+            success: function (data) {
+                addBookData(data)
+                console.log(data)
+            },
+            error: function (xhr, text, error) {
+                console.log(text)
+            }
+        })
+    })
+}
+
+// search()
+books.push({ queryType: "isbn", queryValue: 9789814561778 })
+books.push({ queryType: "title", queryValue: "How to Win Friends and Influence People" })
+    //fetch()
---
+++ exercises/week-6/ajax-Intro/ex45.js
@@ -0,0 +1,27 @@
+// Exercise 4+5
+const searchGif = function (cats) {
+    const key = `RI1CIXCll0k2LGXsHrH0DRkv859fx6vX&limit=5`
+    let xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${cats}&api_key=${key}`)
+    xhr.done(function (data) {
+        let firstGif = data.data[0]
+
+        if (firstGif) {
+            let embedUrl = firstGif.embed_url
+            let $iframe = $(`<iframe>`)
+            $iframe.attr(`src`, embedUrl)
+            $(`body`).append($iframe)
+        }
+    })
+}
+
+$(document).ready(function () {
+    const input = $(`<input>`).attr(`type`, `text`).attr(`id`, `requestInput`).attr(`placeholder`, `Get gif about...`)
+    const button = $(`<button>`).attr(`id`, `searchButton`).text(`Get gif about...`)
+
+    $(`body`).append(input, button)
+
+    $(`#searchButton`).click(function () {
+        let inputValue = $(`#requestInput`).val()
+        searchGif(inputValue)
+    })
+})
