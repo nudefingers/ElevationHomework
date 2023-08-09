1c7cca99dfc2f4d3d387d9d964ca9f78d68423bb
nudefingers <nude.fingers@gmail.com>
2023-08-09 18:30:46 +0300

    done ex-2

--- exercises/week-10/react-input/src/components/Exercise2.js
+++ exercises/week-10/react-input/src/components/Exercise2.js
@@ -4,10 +4,20 @@ const Exercise2 = () => {
     const [name, setName] = useState("")
     const [fruit, setFruit] = useState("")
 
+    const handleFruitChange = (event) => {
+        const changedFruit = event.target.value
+        setFruit(changedFruit)
+        console.log(changedFruit)
+    }
+
     return (
         <div>
             <input id="name-input" onChange={(e) => setName(e.target.value)} value={name} />
-            <select id="select-input" onChange={(e) => setFruit(e.target.value)} value={fruit}>
+            <select id="select-input" onChange={handleFruitChange} value={fruit}>
+                <option value="apple">Apple</option>
+                <option value="banana">Banana</option>
+                <option value="orange">Orange</option>
+                <option value="grape">Grape</option>
             </select>
         </div>
     );
